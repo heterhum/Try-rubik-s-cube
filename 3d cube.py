5f03669449845b213897aff7bcb81559ddd2f31f
heterhum <160785637+heterhum@users.noreply.github.com>
2024-12-02 23:33:02 +0100

    a

--- 3d cube.py
+++ 3d cube.py
@@ -20,3 +20,26 @@ def draw():
     #plane(300,300)
 
 run(mode="P3D",renderer="vispy")
+
+#from p5 import *
+#l=[[(0, 255, 0),(0, 255, 0),(0, 255, 0)],[(0, 255, 0),(0, 255, 0),(0, 255, 0)],[(0, 255, 0),(0, 255, 0),(0, 255, 0)]]
+#STEP=100
+#first=True
+#
+#def setup():
+#    size(600, 600)
+#    background(100)   
+#
+#def draw():
+#    global first
+#    background(100)   
+#    rotateX(mouse_y/100)
+#    rotateY(mouse_x/100)
+#    
+#    for y in range(2):
+#        for x in range(2):
+#            translate(STEP*x,STEP*y)
+#            fill(l[y][x][0],l[y][x][1],l[y][x][2])
+#            plane(STEP,STEP)
+#
+#run(mode="P3D",renderer="vispy")
